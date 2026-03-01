d2b98657d71c9ab082e0662154f710a97c690f79
michael <mm.e.mihon@gmail.com>
2026-03-01 20:50:06 +0300

    refactor: lifespan main

--- services/server/app/main.py
+++ services/server/app/main.py
@@ -1,12 +1,23 @@
 """FastAPI приложение — точка входа в приложение."""
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.database import engine
 from app.models import Base
 
 from .api.routes import user, task
 
-# Создание таблиц БД при старте
-Base.metadata.create_all(bind=engine)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # --- startup ---
+    
+    # Создание таблиц БД при старте
+    Base.metadata.create_all(bind=engine)
+    # TODO: тут же можно запустить планировщик напоминаний / воркер, если он будет
+
+    yield
+    # --- shutdown ---
+    # TODO: остановить планировщик/воркер, если запускали выше
+    ...
 
 app = FastAPI()
 
