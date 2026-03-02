5d9239eb00e0f69574e2f44ac029e7dfe7eb4cd7
michael <mm.e.mihon@gmail.com>
2026-03-02 20:01:24 +0300

    refactor: async db init

--- services/server/app/main.py
+++ services/server/app/main.py
@@ -3,19 +3,23 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.database import engine
 from app.models import Base
+import asyncio
 
 from .api.routes import user, task
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # --- startup ---
-    
     # Создание таблиц БД при старте
-    Base.metadata.create_all(bind=engine)
+    def init_db():
+        Base.metadata.create_all(bind=engine)
+    
+    await asyncio.to_thread(init_db)  # Запускаем в отдельном потоке
     # TODO: тут же можно запустить планировщик напоминаний / воркер, если он будет
 
     yield
     # --- shutdown ---
+    engine.dispose()  # Закрыть все соединения
     # TODO: остановить планировщик/воркер, если запускали выше
     ...
 
